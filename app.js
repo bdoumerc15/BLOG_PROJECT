09686de21de1367aee488fd7dd8ddff7d378e3ea
bladimir doumerc <bdoumerc15@gmail.com>
2021-01-05 23:28:29 -0700

    Se ha mejorado un poco de diseño a nuestro proyecto

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ import bodyParser from 'body-parser'
 import express from 'express'
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    const file = `${process.cwd()}\\index.html`
+    const file = `${process.cwd()}\\index.html`;
     res.sendFile(file);
 })
 
